396922720fbafbf9e63f3fea519b14cb3ed255bb
nafidurmus <nafidurmus07@gmail.com>
2018-07-15 14:17:50 +0300

    Add more price

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ class HomeController < ApplicationController
 		@news = JSON.parse(@response)
 
 		#Grab Price Data
-		@prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD'
+		@prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XLM,MIOTA,USDT,TRX&tsyms=USD,TRY'
 		@prices_uri = URI(@prices_url)
 		@prices_response = Net::HTTP.get(@prices_uri)
 		@prices = JSON.parse(@prices_response)
